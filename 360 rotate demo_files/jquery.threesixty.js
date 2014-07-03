923d896e06a30755b426be2e744fa4328eb18a25
up2u-admob <up2u.admob@gmail.com>
2014-07-03 13:22:51 +0900

    Update jquery.threesixty.js

--- 360 rotate demo_files/jquery.threesixty.js
+++ 360 rotate demo_files/jquery.threesixty.js
@@ -156,7 +156,7 @@ jQuery.fn.threesixty = function(options){
 		//		pic.data("enabled","1"); 	
 		//	});
 			
-			pic.bind('touchstart mousedown', function(e) {
+			pic.bind('mousedown', function(e) {
 				 e.preventDefault();        
 				 pic.data("enabled","1"); 
 			});
@@ -167,7 +167,7 @@ jQuery.fn.threesixty = function(options){
 		//		pic.data("currentIndex",pic.data("tempIndex"));
 		//	});
 			
-			$("body").bind('touchend mouseup', function(e) {
+			$("body").bind('mouseup', function(e) {
 	 			e.preventDefault();
 	 			pic.data("enabled","0");
 				pic.data("currentIndex",pic.data("tempIndex"));
